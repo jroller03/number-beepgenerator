8c75f7af04ec8d13ee07acc0372f1c6c7671ea25
Justin Roller <justin@>
2018-04-10 15:14:35 -0700

    seperated business logic and user logic in js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,26 @@
-//scripts to convert user input into phrases if not then give range of numbers.
-
+// business logic
   var userInput = 0;
   var initialArray=[];
 
 
-
+// user interface logic
   $(document).ready(function() {
     $("#formOne").submit(function(event) {
       userInput = parseInt($("#userInput").val());
-
-
-
         for(var i=0; i<=userInput; i++){
           var stringNumber=i.toString();
-          var oneIndex=stringNumber.indexOf("1");
-          var zeroIndex=stringNumber.indexOf("0");
-
-
+          var one=stringNumber.indexOf("1");
+          var zero=stringNumber.indexOf("0");
           if (i===0) {
             initialArray.push("Beep!");
           }
           else if (i%3===0) {
             initialArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
           }
-          else if (oneIndex!=-1) {
+          else if (one!=-1) {
             initialArray.push("Boop!");
           }
-          else if (zeroIndex!=-1) {
+          else if (zero!=-1) {
             initialArray.push("Beep!");
           }
           else {
@@ -35,10 +29,6 @@
         }
         console.log(initialArray);
         event.preventDefault();
-
-
-
-
       $("#result").text(initialArray);
       $("#result").show();
     });
